b79db67f1140cf9ba121a4f57fd6f1a8f3ecd229
Aditya Atul Gawali <aditya06.c@quickheal.com>
2025-09-29 17:53:06 +0530

    cart sercise initial commit after cleaning the code

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 import express from "express"
 import cors from "cors"
 import { connectDB } from "./config/db.js"
-import userRouter from "./routes/userRoute.js"
 import cartRouter from "./routes/cartRoute.js"
-import orderRouter from "./routes/orderRoute.js"
-import dishRouter from "./routes/dishRoute.js"
+
 
 import dotenv from 'dotenv';
 dotenv.config(); // Ensure this is at the very top
@@ -19,14 +17,10 @@ connectDB();
 
 
 // api endpoint
-app.use("/api/dish", dishRouter)
-app.use("/images", express.static('uploads'))
-app.use("/api/user", userRouter)
 app.use("/api/cart", cartRouter)
-app.use("/api/order", orderRouter)
 
-app.get("/", (req, res) => {
-    res.send("API Working ")
+app.get("/api/cart", (req, res) => {
+    res.send("cart service API is Working ")
 })
 
 app.listen(port, () => {
