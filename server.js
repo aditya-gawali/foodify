a2fe450a7603b5530b498ed5348f6c2fb648d3e1
Aditya Atul Gawali <aditya06.c@quickheal.com>
2025-09-29 16:50:31 +0530

    user service initial code after cleaning the code

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ import express from "express"
 import cors from "cors"
 import { connectDB } from "./config/db.js"
 import userRouter from "./routes/userRoute.js"
-import cartRouter from "./routes/cartRoute.js"
-import orderRouter from "./routes/orderRoute.js"
-import dishRouter from "./routes/dishRoute.js"
+
 
 import dotenv from 'dotenv';
 dotenv.config(); // Ensure this is at the very top
@@ -19,14 +17,11 @@ connectDB();
 
 
 // api endpoint
-app.use("/api/dish", dishRouter)
-app.use("/images", express.static('uploads'))
 app.use("/api/user", userRouter)
-app.use("/api/cart", cartRouter)
-app.use("/api/order", orderRouter)
 
-app.get("/", (req, res) => {
-    res.send("API Working ")
+
+app.get("/api/user", (req, res) => {
+    res.send("User API is Working ")
 })
 
 app.listen(port, () => {
